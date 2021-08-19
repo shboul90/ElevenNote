cae51bc2980ce704277a452bc8e6859a632b471f
Mohamad Al-Shboul <Shboul90@gmail.com>
2021-08-19 19:41:08 -0400

    AddedNotes

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -20,6 +23,22 @@ namespace ElevenNote.Data
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+        {
+            public IdentityUserLoginConfiguration()
+            {
+                HasKey(iul => iul.UserId);
+            }
+        }
+
+        public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+        {
+            public IdentityUserRoleConfiguration()
+            {
+                HasKey(iur => iur.UserId);
+            }
+        }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -28,6 +47,19 @@ namespace ElevenNote.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet<Note> Notes { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add (new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
         }
     }
 }
