cdc2095fa16bcf77ef992ffc868220dbbc8cd5ec
Mohamad Al-Shboul <Shboul90@gmail.com>
2021-08-23 22:44:51 -0400

    4-8

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -39,5 +39,13 @@ namespace ElevenNote.WebAPI.Controllers
 
             return Ok();
         }
+
+        public IHttpActionResult Get(int id)
+        {
+            NoteService noteService = CreateNoteService();
+            var note = noteService.GetNoteById(id);
+            return Ok(note);
+        }
+
     }
 }
