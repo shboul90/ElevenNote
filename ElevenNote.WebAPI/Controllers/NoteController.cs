a89b0cc06323ab7e4263c24712b092488c5a981c
Mohamad Al-Shboul <Shboul90@gmail.com>
2021-08-23 22:51:18 -0400

    4-11

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -47,5 +47,18 @@ namespace ElevenNote.WebAPI.Controllers
             return Ok(note);
         }
 
+        public IHttpActionResult Put(NoteEdit note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.UpdateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+
     }
 }
