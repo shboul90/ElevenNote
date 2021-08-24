b630f6e63d6ee20aaf5e8746ded1b2afd61f5a0b
Mohamad Al-Shboul <Shboul90@gmail.com>
2021-08-23 23:42:11 -0400

    complete

---
+++ ElevenNote.Data/Migrations/202108240340187_InitialMigration.cs
@@ -0,0 +1,18 @@
+﻿namespace ElevenNote.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Note", "Title", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Note", "Title");
+        }
+    }
+}
